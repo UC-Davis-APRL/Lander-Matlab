bfd5350c3ded7aee8a51bb79b9f03a412f03854a
jtwright206 <jtwr@ucdavis.edu>
2025-02-11 08:00:30 -0800

    Rewriting dynamics block

--- PositionLQR.m
+++ PositionLQR.m
@@ -1,18 +1,36 @@
-function K = PositionLQR()
-    m = 1; % mass of rocket
-    A = [0, 0, 0, 1, 0, 0;
-        0, 0, 0, 0, 1, 0;
-        0, 0, 0, 0, 0, 1;
-        0, 0, 0, 0, 0, 0;
-        0, 0, 0, 0, 0, 0;
-        0, 0, 0, 0, 0, 0];
-    B = [0, 0, 0;
-        0, 0, 0;
-        0, 0, 0;
-        1/m, 0, 0;
-        0, 1/m, 0;
-        0, 0, 1/m]; % Need to flip z-axis force to upward
-    Q = diag([1, 1, 1.5, 1, 1, 1]); % Can test later
-    R = 1; % Don't care, something with elec. power
-    K = lqr(A, B, Q, R); % Compute optimal gain matrix K
-end
+m = 1; % mass of rocket
+A = [0, 0, 0, 1, 0, 0;
+    0, 0, 0, 0, 1, 0;
+    0, 0, 0, 0, 0, 1;
+    0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0];
+B = [0, 0, 0;
+    0, 0, 0;
+    0, 0, 0;
+    1/m, 0, 0;
+    0, 1/m, 0;
+    0, 0, 1/m]; % Need to flip z-axis force to upward
+Q = diag([1, 1, 1.5, 1, 1, 1]); % Can test later
+R = 1; % Don't care, something with elec. power
+K1 = lqr(A, B, Q, R); % Compute optimal gain matrix K
+
+Ix = 1; % Still need values from two string method
+Iy = 1; % Still need values from two string method
+Iz = 1; % Still need values from two string method
+A = [0, 0, 0, 1, 0, 0;
+    0, 0, 0, 0, 1, 0;
+    0, 0, 0, 0, 0, 1;
+    0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0];
+B = [0, 0, 0;
+    0, 0, 0;
+    0, 0, 0;
+    1/Ix, 0, 0;
+    0, 1/Iy, 0;
+    0, 0, 1/Iz];
+Q = diag([1, 1, 1, 1, 1, 1]); % Can test later
+R = 1; % Don't care, something with elec. power
+K2 = lqr(A, B, Q, R); % Compute optimal gain matrix K
+
