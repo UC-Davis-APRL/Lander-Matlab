0a02c4d3a0a53d8eb76b0bab2441108f3782f307
Alex Brian Zamora <149623024+abrzamora@users.noreply.github.com>
2025-04-17 21:01:26 -0700

    added init code

---
+++ init.m
@@ -0,0 +1,26 @@
+% close all plots
+close all
+clc
+
+
+% run constants.m
+run("constants.m");
+
+close all
+
+
+%% Simulation initial conditions
+%initial states for plant model
+
+%initial state for estimator
+
+%target states for controller
+
+
+%list of waypoints
+waypoints = [0, 0 ,0];
+
+tol = 0.1; %tolerance when waypoint is considered "reached"
+
+%% Test parameters 
+% simulation parameters
